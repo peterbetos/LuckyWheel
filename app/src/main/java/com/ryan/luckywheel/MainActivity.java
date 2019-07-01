902eac5c60e99f0ce43dfa0daca1dc34094b9eca
dharel.h.rosell <dharel.h.rosell@accenture.com>
2019-07-01 12:21:56 +0800

    Removing the duplicate library in app module

--- app/src/main/java/com/ryan/luckywheel/MainActivity.java
+++ app/src/main/java/com/ryan/luckywheel/MainActivity.java
@@ -9,14 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import library.LuckyWheelView;
-import library.model.LuckyItem;
-
-//import rubikstudio.library.LuckyWheelView;
-//import rubikstudio.library.model.LuckyItem;
-//import rubikstudio.library.PielView;
-
-
+import rubikstudio.library.LuckyWheelView;
+import rubikstudio.library.model.LuckyItem;
 
 public class MainActivity extends Activity {
     List<LuckyItem> data = new ArrayList<>();
@@ -30,38 +24,38 @@ public class MainActivity extends Activity {
         final LuckyWheelView luckyWheelView = (LuckyWheelView) findViewById(R.id.luckyWheel);
 
         LuckyItem luckyItem1 = new LuckyItem();
-        luckyItem1.topText = "0";
+        luckyItem1.topText = "100";
         luckyItem1.icon = R.drawable.test1;
         luckyItem1.color = 0xffFFF3E0;
         data.add(luckyItem1);
 
         LuckyItem luckyItem2 = new LuckyItem();
-        luckyItem2.topText = "1";
+        luckyItem2.topText = "200";
         luckyItem2.icon = R.drawable.test2;
         luckyItem2.color = 0xffFFE0B2;
         data.add(luckyItem2);
 
         LuckyItem luckyItem3 = new LuckyItem();
-        luckyItem3.topText = "2";
+        luckyItem3.topText = "300";
         luckyItem3.icon = R.drawable.test3;
         luckyItem3.color = 0xffFFCC80;
         data.add(luckyItem3);
 
         //////////////////
         LuckyItem luckyItem4 = new LuckyItem();
-        luckyItem4.topText = "3";
+        luckyItem4.topText = "400";
         luckyItem4.icon = R.drawable.test4;
         luckyItem4.color = 0xffFFF3E0;
         data.add(luckyItem4);
 
         LuckyItem luckyItem5 = new LuckyItem();
-        luckyItem5.topText = "4";
+        luckyItem5.topText = "500";
         luckyItem5.icon = R.drawable.test5;
         luckyItem5.color = 0xffFFE0B2;
         data.add(luckyItem5);
 
         LuckyItem luckyItem6 = new LuckyItem();
-        luckyItem6.topText = "5";
+        luckyItem6.topText = "600";
         luckyItem6.icon = R.drawable.test6;
         luckyItem6.color = 0xffFFCC80;
         data.add(luckyItem6);
@@ -69,39 +63,39 @@ public class MainActivity extends Activity {
 
         //////////////////////
         LuckyItem luckyItem7 = new LuckyItem();
-        luckyItem7.topText = "6";
+        luckyItem7.topText = "700";
         luckyItem7.icon = R.drawable.test7;
         luckyItem7.color = 0xffFFF3E0;
         data.add(luckyItem7);
 
         LuckyItem luckyItem8 = new LuckyItem();
-        luckyItem8.topText = "7";
+        luckyItem8.topText = "800";
         luckyItem8.icon = R.drawable.test8;
         luckyItem8.color = 0xffFFE0B2;
         data.add(luckyItem8);
 
 
         LuckyItem luckyItem9 = new LuckyItem();
-        luckyItem9.topText = "8";
+        luckyItem9.topText = "900";
         luckyItem9.icon = R.drawable.test9;
         luckyItem9.color = 0xffFFCC80;
         data.add(luckyItem9);
         ////////////////////////
 
         LuckyItem luckyItem10 = new LuckyItem();
-        luckyItem10.topText = "9";
+        luckyItem10.topText = "1000";
         luckyItem10.icon = R.drawable.test10;
         luckyItem10.color = 0xffFFE0B2;
         data.add(luckyItem10);
 
         LuckyItem luckyItem11 = new LuckyItem();
-        luckyItem11.topText = "10";
+        luckyItem11.topText = "2000";
         luckyItem11.icon = R.drawable.test10;
         luckyItem11.color = 0xffFFE0B2;
         data.add(luckyItem11);
 
         LuckyItem luckyItem12 = new LuckyItem();
-        luckyItem12.topText = "11";
+        luckyItem12.topText = "3000";
         luckyItem12.icon = R.drawable.test10;
         luckyItem12.color = 0xffFFE0B2;
         data.add(luckyItem12);
@@ -110,9 +104,6 @@ public class MainActivity extends Activity {
 
         luckyWheelView.setData(data);
         luckyWheelView.setRound(5);
-        float targetAngle = ((360f * 5) + 270f - getAngleOfIndexTarget(7) - (360f / data.size()) / 2);
-        luckyWheelView.setInitialAngle(targetAngle);
-
 
         /*luckyWheelView.setLuckyWheelBackgrouldColor(0xff0000ff);
         luckyWheelView.setLuckyWheelTextColor(0xffcc0000);
@@ -123,12 +114,8 @@ public class MainActivity extends Activity {
         findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                int index = getRandomIndex();
-//                luckyWheelView.startLuckyWheelWithTargetIndex(index);
-                int index = 0;
-                Toast.makeText(getApplicationContext(), "stop clicked", Toast.LENGTH_SHORT).show();
-                luckyWheelView.setPredeterminedNumber(index);
-                luckyWheelView.stopRotation();
+                int index = getRandomIndex();
+                luckyWheelView.startLuckyWheelWithTargetIndex(index);
             }
         });
 
@@ -154,9 +141,5 @@ public class MainActivity extends Activity {
     private int getRandomRound() {
         Random rand = new Random();
         return rand.nextInt(10) + 15;
-    }
-
-    private float getAngleOfIndexTarget(int index) {
-        return (360f / data.size()) * index;
     }
 }
